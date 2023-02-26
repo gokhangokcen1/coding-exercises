5611553f950244682f36b6b2ffe48cf5d68ebb00
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-26 20:33:00 +0300

    Create main.c

---
+++ codechef/squats/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x;
+	    scanf("%d",&x);
+	    printf("%d\n",x*15);
+
+
+			
+}
+	return 0;
+}
+
