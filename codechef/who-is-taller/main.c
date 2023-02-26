d5a7574db8fe55a6a9e9768cb452b3b5c67e273b
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-26 21:07:35 +0300

    Create main.c

---
+++ codechef/who-is-taller/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    if(a>b)
+	    	printf("A\n");
+	    else
+	    	printf("B\n");
+			    
+}		
+	return 0;
+}
+
