634c18bfc077fbb1d26f9773423c62c7584eaa5c
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-27 16:39:58 +0300

    Create main.c

---
+++ codechef/max-minus-min/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y,z;
+	    scanf("%d %d %d",&x,&y,&z);
+		printf("%d\n",z-x);
+
+}		
+	return 0;
+}	
+
