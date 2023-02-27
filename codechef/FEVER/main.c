956fa7768504be7fb1b407b45472a9ce2e6f257a
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-27 16:46:32 +0300

    Create main.c

---
+++ codechef/FEVER/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x;
+	    scanf("%d",&x);
+		if(x>98)
+			printf("YES\n");
+		else
+			printf("NO\n");
+}		
+	return 0;
+}	
+
