b12fcebaa9f11f40c6fe134f160bfee1fc89d300
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-27 15:54:28 +0300

    Create main.c

---
+++ codechef/number-of-credits/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y,z;
+	    scanf("%d %d %d",&x,&y,&z);
+	    printf("%d\n",(4*x)+(2*y)+(0*z));
+
+	    	
+
+}		
+	return 0;
+}
+
