40155cd7b36a67455b327f7ddb00099f2e208502
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-28 14:00:14 +0300

    Create main.c

---
+++ codechef/PARTY2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y,z;
+	    scanf("%d %d %d",&x,&y,&z);
+		if((x*y)<=z)
+			printf("YES\n");
+		else
+			printf("NO\n");
+
+}		
+	return 0;
+}	
+
