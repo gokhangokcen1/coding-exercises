a7f9848a6cae3ac6ba28ed774823737a1fc011b9
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-28 19:02:28 +0300

    Create main.c

---
+++ codechef/TABLETS/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+		if(y>=(x*3))
+			printf("YES\n");
+		else
+			printf("NO\n");
+			
+}		
+	return 0;
+}	
+
