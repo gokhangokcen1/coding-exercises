c91feb0d9f9788a96046996beb458415b6913e6f
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-28 19:27:36 +0300

    Create main.c

---
+++ codechef/DISCNT/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x;
+	    scanf("%d",&x);
+	    printf("%d\n",100-x);
+			
+}		
+	return 0;
+}	
+
