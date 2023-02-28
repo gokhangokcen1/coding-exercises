17b4b89ccc8426c0d066bd4cd35379dfb951745f
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-28 19:32:21 +0300

    Create main.c

---
+++ codechef/HEIGHTRATION/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int x,y;
+	scanf("%d %d",&x,&y);
+	if(x>=y)
+	    printf("%d\n",x);
+    else
+	    printf("%d\n",y);
+	return 0;
+}
+
