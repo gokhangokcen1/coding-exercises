3e9590bf10a1d651ee05813d36679ef034ce3cd5
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-02-28 14:10:45 +0300

    Create main.c

---
+++ codechef/TCKTFINE/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y,z;
+	    scanf("%d %d %d",&x,&y,&z);
+		printf("%d\n",(y-z)*x);				
+}		
+	return 0;
+}	
+
