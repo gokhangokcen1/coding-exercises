5d380b09e651b3af51048f99872b0c7b2fd87824
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-03-03 13:25:59 +0300

    Create main.c

---
+++ codechef/TYRE/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+		printf("%d\n",(2*x)+(4*y));			
+}		
+	return 0;
+}	
+
