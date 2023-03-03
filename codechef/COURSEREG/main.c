2cad6c6ca8a957d257881e6919eff2bc0f956877
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-03-03 13:44:13 +0300

    Create main.c

---
+++ codechef/COURSEREG/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i,t;
+	scanf("%d",&t);
+	
+	for(i=0;i<t;i++){
+	    int x,y;
+	    scanf("%d %d",&x,&y);
+	    if(x<=y)
+	    	printf("%d\n",x);
+	    else
+	    	printf("%d\n",y);
+	    
+	    
+		
+}		
+	return 0;
+}	
+
