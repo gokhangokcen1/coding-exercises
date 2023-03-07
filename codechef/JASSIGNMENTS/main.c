5737a8bb6f663cbc50bcfb784888ff9365a7b2ae
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2023-03-07 15:07:14 +0300

    Create main.c

---
+++ codechef/JASSIGNMENTS/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int i,t;
+	scanf("%d", &t);
+
+	for(i=0;i<t;i++){
+		int x;
+		scanf("%d",&x);
+		if((x+3)<=10)
+			printf("Yes\n");
+		else
+			printf("No\n");
+		
+
+	}
+
+}
+
+
